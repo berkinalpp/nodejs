6016ce3245433aa250706747738038a2c4e987f6
Berkin Alp <berkinalpp@gmail.com>
2022-01-26 20:12:20 +0300

    Clean blog homework 3 completed

--- 06-clean-blog/app.js
+++ 06-clean-blog/app.js
@@ -1,18 +1,30 @@
 import express from "express";
+import mongoose from 'mongoose'
+import Post from './models/Post.js'
 const app = express();
+
+//MONGODB CONNECT
+mongoose.connect('mongodb://localhost:27017/cleanblog-test-db' , (err) => {
+  if(err) console.log(err)
+
+  console.log('MongoDB Connected')
+})
+
 
 //TEMPLATE ENGINE
 
 app.set('view engine','ejs')
 
-//STATIC FILES
+//MIDDLEWARES
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
 
 //ROUTES
-app.get("/", (req, res) => {
-
-  res.render('index')
+app.get("/", async(req, res) => {
+  const posts = await Post.find({})
+  res.render('index' , {posts})
 
 });
 
@@ -33,6 +45,13 @@ app.get("/about", (req, res) => {
   res.render('about')
 
 });
+
+app.post('/posts',async(req, res) =>{
+    await Post.create(req.body)
+    res.redirect('/')
+})
+
+
 
 app.listen(3000, () => {
   console.log("Server running on 3000 port");
