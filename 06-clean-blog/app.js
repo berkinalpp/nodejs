34b318d2800a427fdb3221a1aba21f3766f5df86
Berkin Alp <berkinalpp@gmail.com>
2022-01-27 16:56:05 +0300

    Clean blog homework 4 completed

--- 06-clean-blog/app.js
+++ 06-clean-blog/app.js
@@ -28,9 +28,9 @@ app.get("/", async(req, res) => {
 
 });
 
-app.get("/post", (req, res) => {
-
-  res.render('post')
+app.get("/post/:id", async(req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post',{post})
 
 });
 
